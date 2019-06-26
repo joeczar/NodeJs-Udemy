42dbf1a9b38aff16b4ac253a42865002aeae8b76
Joe Czarnecki <joe.czar@gmail.com>
2019-06-26 12:48:31 +0200

    Installed validator

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,15 +1,9 @@
 /*jshint esversion: 6 */
-/* 
-Challenge: Define 6 use a function in  a new file.
-
-1. Create a new File  called notes.js
-2. Create getNotes function that returns "Your Notes---"
-3. Export getNotes function
-4. Load in and call getNotes from app.js and console.log message
-
-*/
-
+const validator = require('validator');
 const getNotes = require('./notes.js');
 
 getNotes();
-//console.log(add(60, 9));require('./notes.js').default
+
+console.log(validator.isEmail('joe.czaroutlook.com'));
+
+console.log(validator.isURL('https://joeczarnecki.com'));
