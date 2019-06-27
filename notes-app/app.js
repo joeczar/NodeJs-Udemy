8336771b77785f5ce8c006e85627a8f898dc8b73
Joe Czarnecki <joe.czar@outlook.com>
2019-06-27 15:27:06 +0200

    19. added add note function

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,7 +2,7 @@
 const validator = require('validator');
 const chalk = require('chalk');
 const yargs = require('yargs');
-const getNotes = require('./notes.js');
+const notes = require('./notes.js');
 
 //Customize Yargs version
 yargs.version('1.1.0');
@@ -24,8 +24,7 @@ yargs.command({
     },
   },
   handler: function (argv) {
-    console.log('Title: ' + argv.title);
-    console.log('Body: ' + argv.body);
+    notes.addNote(argv.title, argv.body);
     
   }
 });
