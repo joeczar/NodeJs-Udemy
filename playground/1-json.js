3b39108bc31cd18b44d9c42ad42b5241e1ead74a
Joe Czarnecki <joe.czar@gmail.com>
2019-06-27 14:54:39 +0200

    Added playground & learned about parsing JSON data

---
+++ playground/1-json.js
@@ -0,0 +1,27 @@
+/*jshint esversion: 6 */
+
+const fs = require('fs');
+
+// const book = {
+//   title: 'Ego is the enemy',
+//   author: 'Ryan Holiday'
+// };
+
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync('1-JSON.json', bookJSON)
+
+// const dataBuffer = fs.readFileSync('1-json.json');
+// const dataJSON = dataBuffer.toString();
+
+// const data = JSON.parse(dataJSON);
+// console.log(data.title);
+
+const dataBuffer = fs.readFileSync('1-json.json');
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+
+data.name = 'Joe';
+data.age = 38;
+
+const dataString = JSON.stringify(data);
+fs.writeFileSync('1-json.json', dataString);
