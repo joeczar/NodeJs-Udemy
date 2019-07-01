085db36d5bffd4b02ffb09ff4797925e7cc55992
Joe Czarnecki <joe.czar@outlook.com>
2019-07-01 13:09:42 +0200

    34. Handling errors

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,35 +1,43 @@
 /*jshint esversion: 6 */
-const request = require('request');
+const request = require("request");
 
-const getWeather = (locationData) => {
- 
-  const url = 'https://api.darksky.net/forecast/cb3bd684c5147594a33daa61c14c185b/52.563873,%2013.426779?units=si&lang=de';
+// const url =
+//   "https://api.darksky.net/forecast/cb3bd684c5147594a33daa61c14c185b/52.563873,%2013.426779?units=si&lang=de";
 
-  request({ url: url, json: true }, (error, response) => {
-    const dataCurrent = response.body.currently;
-    console.log(`Es ist ${dataCurrent.temperature} grad, mit eine ${dataCurrent.precipProbability} prozent Niederschlagswarscheinlichkeit.`);
-    const dataDaily = response.body.daily.data[0].summary;
-    console.log(dataDaily);
-});
-};
+// request({ url: url, json: true }, (error, response) => {
+//   if (error) {
+//     console.log("unable to connect to weather service.");
+//   } else if (response.body.error) {
+//     console.log(response.body.error);
 
-//  MapBox 
+//   } else {
+//     const dataCurrent = response.body.currently;
+//     console.log(
+//       `Es ist ${dataCurrent.temperature} grad, mit eine ${
+//         dataCurrent.precipProbability
+//       } prozent Niederschlagswarscheinlichkeit.`
+//     );
+//     const dataDaily = response.body.daily.data[0].summary;
+//     console.log(dataDaily);
+//   }
+// });
 
-const getLocation = (locationName) => {
-  //const key = 'pk.eyJ1IjoiYXhpb21zaGlmdCIsImEiOiJjanhqYXpjeWgxY3cxM3lta2c1OW1hamV0In0.6KyJtBkWKGlgN-CEBdNvwg';
-  const key = 'pk.eyJ1IjoiYXhpb21zaGlmdCIsImEiOiJjanhqOTVtdWcxeHh6M25zODAwMnJobTN6In0.uwQ6n54FQh2O7L38_VMtAg';
-  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${locationName}.json?access_token=${key}&limit=1`;
+//  MapBox
+const locationName = "Neukölln";
+//const key = 'pk.eyJ1IjoiYXhpb21zaGlmdCIsImEiOiJjanhqYXpjeWgxY3cxM3lta2c1OW1hamV0In0.6KyJtBkWKGlgN-CEBdNvwg';
+const key =
+  "pk.eyJ1IjoiYXhpb21zaGlmdCIsImEiOiJjanhqOTVtdWcxeHh6M25zODAwMnJobTN6In0.uwQ6n54FQh2O7L38_VMtAg";
+const geoLocUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${locationName}.json?access_token=${key}&limit=1`;
 
-  request({ url: url, json: true}, (error, response) => {
-    //console.log(response.body);
+request({ url: geoLocUrl, json: true }, (error, response) => {
+  if (error) {
+    console.log("Couldn't reach MapBox.");
+  } else if (response.body.features.length < 1) {
+    console.log('No matching results');
+  } else {
     const placeName = response.body.features[0].place_name;
     const long = response.body.features[0].center[0];
     const lat = response.body.features[0].center[1];
-    
     console.log(placeName, long, lat);
-    
-  });
-  
-  
-};
-getLocation('Prenzlauer Promenade 147 Berlin');
+  }
+});
