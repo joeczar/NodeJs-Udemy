6c40aeab787038f7b2f5b6eebd8e063729c51e0b
Joe Czarnecki <joe.czar@outlook.com>
2019-07-05 14:40:00 +0200

    57. Browser HTTP requests with Fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,15 @@
+/*jshint esversion: 6 */
 console.log('Bring the JS to ya');
+
+fetch('http://localhost:3000/weather?location=!').then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data.location);
+      console.log(data.forecast);
+      
+    }
+    
+  });
+});
