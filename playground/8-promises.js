cdac76bce2b66e623109eb13db41647ad64cf9f1
Joe Czarnecki <joe.czar@outlook.com>
2019-07-15 22:20:16 +0200

    93. Promise chaining

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,14 +1,45 @@
 /*jshint esversion: 6 */
 
-const doWorkPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    //resolve([8, 2, 5]);
-    reject('Nope.');
-  }, 2000);
-});
+// const doWorkPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     //resolve([8, 2, 5]);
+//     reject('Nope.');
+//   }, 2000);
+// });
 
-doWorkPromise.then((result) => {
-  console.log('Success!', result);
-}).catch((error) => {
-  console.log('Error!', error);
+// doWorkPromise.then((result) => {
+//   console.log('Success!', result);
+// }).catch((error) => {
+//   console.log('Error!', error);
+// });
+
+// 93. Promise Chaining
+
+const add = (a, b) => {
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve(a + b);
+    }, 2000);
+  });
+};
+// Bad Promise chaining
+// add(1, 2).then((sum) => {
+//   console.log(sum);
+//   add(sum, 5).then((sum2) => {
+//     console.log(sum2);
+//   }).catch((e) => {
+//     console.log(e);
+//   });
+  
+// }).catch((e) => {
+//   console.log(e);
+// });
+/// Promise chaining: The correct way
+add(1, 1).then((sum) => {
+  console.log(sum);
+  return add(sum, 2);
+}).then((sum2) => {
+  console.log(sum2);
+}).catch((e) => {
+  console.log(e);
 });
